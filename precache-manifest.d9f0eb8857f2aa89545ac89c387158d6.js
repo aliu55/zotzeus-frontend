19ed38de64aa0b3432d5152448e049f77c05edf8
Ashley Liu <aslorange61@gmail.com>
2020-11-15 09:58:33 -0800

    Updates

---
+++ precache-manifest.d9f0eb8857f2aa89545ac89c387158d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3198f93def337317e950f236aa69f25c",
+    "url": "/zotzeus-frontend/index.html"
+  },
+  {
+    "revision": "4edce5dfea557482a146",
+    "url": "/zotzeus-frontend/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "8ade04fca98a5c2d8d2a",
+    "url": "/zotzeus-frontend/static/css/main.c3c2db33.chunk.css"
+  },
+  {
+    "revision": "4edce5dfea557482a146",
+    "url": "/zotzeus-frontend/static/js/2.40bb8108.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/zotzeus-frontend/static/js/2.40bb8108.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ade04fca98a5c2d8d2a",
+    "url": "/zotzeus-frontend/static/js/main.e425010a.chunk.js"
+  },
+  {
+    "revision": "1150568692aae76e577b",
+    "url": "/zotzeus-frontend/static/js/runtime-main.c2079d30.js"
+  }
+]);
